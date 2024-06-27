0cc5849cc9faffccefbc03e461160e50f5ad7196
Susan <suguzmanve@est.utn.ac.cr>
2024-06-26 19:52:43 -0600

    Post funcionando desde tsx + css

--- src/components/postTrainers.tsx
+++ src/components/postTrainers.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "../styles/post.css";
 
 const PostTrainer = () => {
   const [nombre, setNombre] = useState('');
@@ -13,9 +14,9 @@ const PostTrainer = () => {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:5005/trainers', 
+      await axios.post('http://localhost:5005/api/addtrainer', 
       { nombre, apellidos, sexo, residencia, foto_Url });
-      navigate('/');
+      navigate('/listTrainer');
     } catch (error) {
       console.error('Error adding new trainer:', error);
       return(
@@ -25,39 +26,29 @@ const PostTrainer = () => {
   };
 
   return (
-    <div className="" >
-      <h2>Agregar Nuevo Entrenador</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="row" >
-         <div className="col">
-          <input type="text" placeholder="Nombre"     value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+    <div className="pad" >
+      <form className="form-container" onSubmit={handleSubmit}>
+         <div >
+          <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
          </div>
-         <div className="col">
+         <br />
+         <div>
            <input type="text" placeholder="Apellidos"  value={apellidos} onChange={(e) => setApellidos(e.target.value)} required />
         </div>
-        </div> 
-        <div className="row" >
-         <div className="col">
-           <input type="text" placeholder="Sexo"       value={sexo} onChange={(e) => setSexo(e.target.value)} required />
-         </div>
-         <div className="col">
-         <input type="text" placeholder="Residencia" value={residencia} onChange={(e) => setResidencia(e.target.value)} required />
-         </div>
+        <br /> 
+         <div>
+           <input type="text" placeholder="Sexo" value={sexo} onChange={(e) => setSexo(e.target.value)} required />
         </div>
-        <div className="row" > 
-          <div className="col">
+         <br />
+         
+         <input type="text" placeholder="Residencia" value={residencia} onChange={(e) => setResidencia(e.target.value)} required />
+          <br />
+          <br />
+          <div>
             <input type="text" placeholder="Foto URL"   value={foto_Url} onChange={(e) => setFotoUrl(e.target.value)} required />
           </div>
-          <div className="col">
-            <button type="submit">Agregar</button>
-          </div>
-        </div>
-
-      
-        
-       
-       
-        
+          <br />
+            <button type="submit"   >Agregar</button>    
       </form>
     </div>
   );
